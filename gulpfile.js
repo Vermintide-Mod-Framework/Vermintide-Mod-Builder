1aefb399556c2cfb2d01230aa9906b8bf4c975ba
UnShame <mrunshame@gmail.com>
2018-04-28 20:51:25 +0300

    Add gulp task to compile and zip vmb.exe

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp'),
+      zip = require('gulp-zip'),
+      pkg = require('pkg').exec;
+
+gulp.task('compile', function (callback) {
+    pkg(['vmb.js', '--target', 'node8-win-x64'])
+        .then(() => {
+            gulp.src(
+                [
+                    '%%template/**/*',
+                    'vmb.exe',
+                    'mods/'
+                ],
+                { base: '.' }
+            )
+            .pipe(zip('vmb.zip'))
+            .pipe(gulp.dest('.'))
+            .on('end', () => callback())
+            .on('error', (err) =>{
+                console.log(err);
+                callback();
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+            callback();
+        });
+});
