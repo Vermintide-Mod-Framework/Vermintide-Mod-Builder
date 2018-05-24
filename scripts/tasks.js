cb0e8cb294c7bbd56e7c874f3a14b662a2c28618
UnShame <mrunshame@gmail.com>
2018-05-25 00:17:42 +0300

    vmb info documentation

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -24,6 +24,7 @@ let tasks = {
             'vmb open      {<mod_name> | --id <item_id>}\n' +
             'vmb build     [<mod_name1> <mod_name2>...] [--ignore-errors] [--verbose] [--clean] [--id <item_id>] [--no-workshop]\n' +
             'vmb watch     [<mod_name1> <mod_name2>...] [--ignore-errors] [--verbose] [--clean] [--id <item_id>] [--no-workshop]\n' +
+            'vmb info      [<mod_name1> <mod_name2>...] [--cfg]\n' +
             'See README.md for more information.'
         );
         return { exitCode: 0, finished: false };
