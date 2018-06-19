2513d5996d430819f9ed8f54e494266a989443b5
UnShame <mrunshame@gmail.com>
2018-06-19 03:49:52 +0300

    Fix template

--- .template-vmf/scripts/mods/%%name/%%name_data.lua
+++ .template-vmf/scripts/mods/%%name/%%name_data.lua
@@ -3,7 +3,7 @@ local mod = get_mod("%%name")
 -- Everything here is optional. You can remove unused parts.
 return {
 	name = "%%title",                               -- Readable mod name
-	description = mod:localize("%%description"),    -- Mod description
+	description = mod:localize("mod_description"),  -- Mod description
 	is_togglable = true,                            -- If the mod can be enabled/disabled
 	is_mutator = false,                             -- If the mod is mutator
 	mutator_settings = {},                          -- Extra settings, if it's mutator
